e15886570688785a77ad46117518bb032d64562a
Niv Eckhaus <niv.eckhaus@gmail.com>
2025-07-25 19:59:15 +0300

    replaced sentence-coherence task with NaCCS

--- constants.py
+++ constants.py
@@ -33,8 +33,8 @@ MODIFY_CONFIG_INSTRUCTION = f"***You can modify this configuration at the manual
 
 # coding tasks
 SEGMENT_LOCUS_VALENCE = "Segment-Locus-Valence"
-SENTENCE_COHERENCE = "Sentence-Coherence"
-ALL_CODING_TASKS = [SEGMENT_LOCUS_VALENCE, SENTENCE_COHERENCE]
+NARRATIVE_COHERENCE = "Narrative-Coherence"  # TODO: change this name to the right scheme name
+ALL_CODING_TASKS = [SEGMENT_LOCUS_VALENCE, NARRATIVE_COHERENCE]
 DEFAULT_CODING_TASK = ALL_CODING_TASKS[0]
 # parameters defining each task (their values should be defined for each task)
 TASK_DEFINITION = "task_definition"
@@ -63,12 +63,33 @@ SLV_COLOR_CODING_LEGEND = (", ".join([f":{cls_color}-background[{cls}]"
                                      + [f":{vln_color}[{vln}]"
                                         for vln, vln_color in SLV_VALENCE_COLORS.items()]))
 
-# Segment-Coherence (`coh`) coding
-COH_LEVELS_AND_COLORS = {"low": "red", "mid": "orange", "high": "green"}
-COH_FORMATTED_CODES = {f"_{lvl}_": f":gray-background[:{lvl_color}[***\\_{lvl}\\_***]]"
-                       for lvl, lvl_color in COH_LEVELS_AND_COLORS.items()}
+# Narrative-Coherence (`coh`) coding
+COH_LEVELS_AND_COLORS = {"0": "red", "1": "orange", "2": "green", "3": "blue"}
+COH_FORMATTED_CODES = {f": {score}\n": f": :{score_color}[**{score}**]\\\n"  # format for the 3 dims
+                       for score, score_color in COH_LEVELS_AND_COLORS.items()}
+COH_FORMATTED_CODES["Context"] = ":gray-background[**Context**]"
+COH_FORMATTED_CODES["Chronology"] = ":gray-background[**Chronology**]"
+COH_FORMATTED_CODES["Theme"] = ":gray-background[**Theme**]"
+COH_FORMATTED_CODES["Total"] = ":violet-background[**Total**]"
 COH_COLOR_CODING_LEGEND = ", ".join([f":{lvl_color}[{lvl}]"
                                      for lvl, lvl_color in COH_LEVELS_AND_COLORS.items()])
+COH_TASK_DEFINITION = """Each memory should be coded by Narrative Coherence Coding Scheme (NaCCS), which includes scoring 3 coherence-dimensions from 0 to 3, and sum them together for a final score (in the range of 0 to 9).
+The 3 dimensions are: Context, Chronology, Theme. Here is a descriptive guide for scoring each one:
+Dimension #1 - Context: Orienting the narrative in time and space;
+Score 0 - No information about time or location is provided. Example: "I got cavities." - explanation for this example's score: No mention of time or location is provided. 
+Score 1 - Partial information is provided; there is mention of time or location at any level of specificity. Example: "When I first found out that I had more serious asthma...." - explanation for this example's score: Mention of time or place at any level of specificity, in this case it is general time.
+Score 2 - Both time and place are mentioned but no more than one dimension is specific. Example: "I was a small child about age 5. I was held by my parents on a famous bridge." - explanation for this example's score: Both time and place are mentioned but no more than one dimension is specific, age 5 is a specific time and a famous bridge is a general bridge.
+Score 3 - Both time and place are mentioned and both are specific. Example: "I remember when my mom had my brother. I was 2 years and 8 months when she brought him home." - explanation for this example's score: Both time and place are mentioned and both are specific (home is the specific place).
+Dimension #2 - Chronology: Relating components along timeline;
+Score 0 - Narrative consists of a list of actions with minimal or no information about temporal order. Example: "A gorilla. A tiger. An elephant. A giraffe. Balloon." - explanation for this example's score: List of actions with minimal or no information about temporal order.
+Score 1 - Naive listener can place some but not most of the events on a timeline. Fewer than half of the temporally relevant actions can be ordered on a timeline with confidence. Example: "I ate all my frosting out of my bowl. We went to the grocery store and got 'em. And we had to wait until the gingerbread houses were ready." - explanation for this example's score: Fewer than half of the temporally relevant actions can be ordered on a timeline.
+Score 2 - Naive listener can place between 50-75% of the relevant actions on a timeline but cannot reliably order the entire story from start to finish with confidence. Example: "I found a giraffe but the giraffe was sick. And then we looked under a blanket, you know what we found? The baby elephant. I saw fishes. Goldfish. A zebra eating. And then I saw a tiger when doing his (unintelligible) dance. And then I saw the koala jumping around the tree. After the banana he burps." - explanation for this example's score: Can place between 50-75% of the relevant actions on a timeline.
+Score 3 - Naive listener can order almost all (> 75%) of the temporally relevant actions. This includes cases in which the speaker marks deviations from temporal order or repairs a violated timeline. Example: "We won 2-1 and it wasn't - it was sort of our best game, because usually all through the main season we didn't do very well in the games, and then when we went in the tournament we did pretty well. And our team, well on our team, no one really got hurt at the game, so we and the other team didn't get hurt. And lots of times - the one time that the ball went in, the player just went right past the defense and got it in, just like right into the corner. And one of ours was like that, and the other - I can't remember what the other one was like." - explanation for this example's score: Can place more than 75% of the relevant actions on a timeline.
+Dimension #3 - Theme: Maintaining and elaborating on topic;
+Score 0 - The narrative is substantially off topic and/or characterized by multiple digressions that make the topic difficult to identify. No attempt to repair digressions. Example: "I got a dress. Me and Shelley got a dress. And Jessie's gonna get on her computer and send it to us. We're gonna buy it. And buy the shoes with flowers on the side. I got the shoes with the flowers on the side." - explanation for this example's score: The narrative is substantially off topic and/or characterized by multiple digressions that make the topic difficult to identify.
+Score 1 - A topic is identifiable and most of the statements relate to it. The narrative may include minimal development of the topic through causal linkages, or personal evaluations and reactions, or elaborations of actions. Example: "I was a small child about age 5. I was held by my parents on a famous bridge. It was in winter and I was wearing a hat. Then I took off my hat, and threw it outside the bridge, mimicking some sound of the plane." - explanation for this example's score: A topic is identifiable and most of the statements relate to the topic in a consistent manner. However, there is no substantial development.
+Score 2 - The narrative substantially develops the topic. Several instances of causal linkages, and/or interpretations, and/or elaborations of previously reported actions are included. Example: "I was kind of scared because they might have to put that trache in, and I didn't know what it would affect to my asthma, on my lungs. I just didn't know because I was kind of crying and I was kind of scared. And I just didn't like the sound of it. I thought it was going to do something to me like not let me breathe anymore or something. And I just didn't really like it and it just hurt me really bad." - explanation for this example's score: The narrative substantially develops the topic. However, there is no resolution, links to other autobiographical experiences, or self-concept; only a wrap-up statement.
+Score 3 - Narrative includes all the above and a resolution to the story, or links to other autobiographical experiences including future occurrences, or self-concept or identity. Resolution brings closure and provides new information. Example: "It wasn't really the fact of getting into medical school that was thrilling even though it was a good thing but just what I had gone through to get there and all the hard work and the struggle had paid off. And that's because I basically had, for the last four years, I saw myself through school. I've had to like work between two to four jobs and at the same time I'm getting four degrees and two minors and I remember times I'd work like third shift from 11 pm to 7 am and then have to go to chemistry class at 8 am and go through rush hour traffic in the morning and all that, so it just made it all worth it like, when I saw the first letter I just felt good that all the hard work was not in vain. It paid off and it just shows that if you are willing to work hard you can succeed." - explanation for this example's score: The narrative substantially develops the topic. In addition, there is a link to other autobiographical experiences ("because ... I saw myself through school") and self-concept ("when I saw the first letter I just felt good that all the hard work was not in vain")."""
 
 # examples keywords (for few-shots)
 INPUT = "INPUT"
@@ -92,12 +113,15 @@ PARAMETERS_BY_CODING_TASK = {
         FORMATTED_CODES_DICT: SLV_FORMATTED_CODES,
         COLOR_CODING_LEGEND: SLV_COLOR_CODING_LEGEND
     },
-    SENTENCE_COHERENCE: {
-        TASK_DEFINITION: "Each memory should be divided to sentences, and each sentence should be coded to its coherence, which can be high, low or mediocre.",
+    NARRATIVE_COHERENCE: {
+        TASK_DEFINITION: COH_TASK_DEFINITION,
         INPUT_FORMAT_INSTRUCTION: "You will be given as input text written by a patient, describing a memory.",
-        OUTPUT_FORMAT_INSTRUCTION: "For each input you get, you will only output the same input, with additional coding of the coherence for each sentence of the memory. The codings are in the pattern of _coherence_, coming right after the corresponding sentence, where the coherence part can be one of the following: high, low or mid (for mediocre).",
-        PUBLIC_CORRECT_EXAMPLES: [{INPUT: "It was in the middle of the summer. I think it might have been the middle of July, but frankly I'm not quite sure... It happened really fast. Suddenly, I'm not sure how, since it happened without my awareness, but I swear to god (and you have to believe me!), he just came...",
-                                   OUTPUT: "It was in the middle of the summer. _high_ I think it might have been the middle of July, but frankly I'm not quite sure... _mid_ It happened really fast. _high_ Suddenly, I'm not sure how, since it happened without my awareness, but I swear to god (and you have to believe me!), he just came... _low_"}],
+        OUTPUT_FORMAT_INSTRUCTION: "For each input you get, you will only output a score for each dimension and their sum in the following format:\n"
+                                   "Context: X\nChronology: Y\nTheme: Z\nTotal: SUM\n"
+                                   "Where X, Y and Z are a score between 0 and 3, and SUM is their total sum. DON'T OUTPUT ANYTHING ELSE! An example for such output can be:\n"
+                                   "Context: 2\nChronology: 0\nTheme: 3\nTotal: 5",
+        # TODO: PUBLIC_CORRECT_EXAMPLES - needed from Bat Sheva
+        PUBLIC_CORRECT_EXAMPLES: [],
         PUBLIC_INCORRECT_EXAMPLES: [],
         PRIVATE_CORRECT_EXAMPLES_SHEET: None,
         PRIVATE_INCORRECT_EXAMPLES_SHEET: None,
